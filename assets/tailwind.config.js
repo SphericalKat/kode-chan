c0e7a003492dc61cc9a8c2f39904c2185b177818
SphericalKat <amolele@gmail.com>
2021-03-06 17:33:40 +0530

    refactor(ui): add base styles to homepage
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,16 +1,22 @@
 module.exports = {
   purge: [
-    '../lib/**/*.ex',
-    '../lib/**/*.leex',
-    '../lib/**/*.eex',
-    './js/**/*.js'
+    "../lib/**/*.ex",
+    "../lib/**/*.leex",
+    "../lib/**/*.eex",
+    "./js/**/*.js",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "card-grey": "#1F1F24",
+        "body-grey": "#16161A",
+        "accent-green": "#3DFDAF",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
