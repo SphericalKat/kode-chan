72a56537ec789d23fba602a192d73557598c7379
SphericalKat <amolele@gmail.com>
2021-03-06 14:54:41 +0530

    refactor(liveview): display user info on homepage if signed in
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- lib/kode_chan_web/live/page_live.ex
+++ lib/kode_chan_web/live/page_live.ex
@@ -1,6 +1,8 @@
 defmodule KodeChanWeb.PageLive do
   use KodeChanWeb, :live_view
+  alias KodeChan.Accounts
+
   def mount(_params, %{"user_id" => user_id}, socket) do
-    {:ok, assign(socket, current_user: nil)}
+    {:ok, assign(socket, current_user: Accounts.get_user_from_id(user_id))}
   end
 end
